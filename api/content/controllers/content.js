ffe500df95f759cdf11f10f56137140677c0a2aa
Otavio Cesar <o.caesar@hotmail.com>
2022-01-07 15:56:01 -0300

    Atualizando Controller

--- api/content/controllers/content.js
+++ api/content/controllers/content.js
@@ -5,23 +5,4 @@
  * to customize this controller
  */
 
-module.exports = {
-
-    async create(data, { files } = {}) {
-        const validData = await strapi.entityValidator.validateEntityCreation(
-          strapi.models.content,
-          data,
-          { isDraft: isDraft(data, strapi.models.content) }
-        );
-        console.log("Upload Content")
-        const entry = await strapi.query('content').create(validData);
-    
-        if (files) {
-          await strapi.entityService.uploadFiles(entry, files, {
-            model: 'content',
-          });
-          return this.findOne({ id: entry.id });
-        }
-        return entry;
-      },
-};
+module.exports = {};
